d5ea73e55f094ce68ec5f72dd1cff3815a81a2d9
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2020-12-30 15:42:49 +0900

    fix: refactor benchmark
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- singleflight_bench_test.go
+++ singleflight_bench_test.go
@@ -7,46 +7,44 @@ import (
 	"golang.org/x/sync/singleflight"
 )
 
-func BenchmarkSingleflight(b *testing.B) {
-	b.Run("singleflight", func(b *testing.B) {
-		sf := New()
+func Benchmark_singleflight(b *testing.B) {
+	sf := New()
 
-		var (
-			cnt   int64
-			total int64
-		)
+	var (
+		cnt   int64
+		total int64
+	)
 
-		for i := 0; i < b.N; i++ {
-			atomic.AddInt64(&total, 1)
-			_, _, shared := sf.Do("A", func() (interface{}, error) {
-				return nil, nil
-			})
-			if shared {
-				atomic.AddInt64(&cnt, 1)
-			}
+	for i := 0; i < b.N; i++ {
+		atomic.AddInt64(&total, 1)
+		_, _, shared := sf.Do("key", func() (interface{}, error) {
+			return nil, nil
+		})
+		if shared {
+			atomic.AddInt64(&cnt, 1)
 		}
+	}
 
-		b.Logf("singleflight total: %d shared: %d", atomic.LoadInt64(&total), atomic.LoadInt64(&cnt))
-	})
+	b.Logf("singleflight total: %d shared: %d", atomic.LoadInt64(&total), atomic.LoadInt64(&cnt))
+}
 
-	b.Run("golang.org/x/sync/singleflight", func(b *testing.B) {
-		sf := new(singleflight.Group)
+func Benchmark_sync_singleflight(b *testing.B) {
+	sf := new(singleflight.Group)
 
-		var (
-			cnt   int64
-			total int64
-		)
+	var (
+		cnt   int64
+		total int64
+	)
 
-		for i := 0; i < b.N; i++ {
-			atomic.AddInt64(&total, 1)
-			_, _, shared := sf.Do("A", func() (interface{}, error) {
-				return nil, nil
-			})
-			if shared {
-				atomic.AddInt64(&cnt, 1)
-			}
+	for i := 0; i < b.N; i++ {
+		atomic.AddInt64(&total, 1)
+		_, _, shared := sf.Do("key", func() (interface{}, error) {
+			return nil, nil
+		})
+		if shared {
+			atomic.AddInt64(&cnt, 1)
 		}
+	}
 
-		b.Logf("singleflight total: %d shared: %d", atomic.LoadInt64(&total), atomic.LoadInt64(&cnt))
-	})
+	b.Logf("singleflight total: %d shared: %d", atomic.LoadInt64(&total), atomic.LoadInt64(&cnt))
 }
